39d21b6a875768393c0c77582e46322cb700cef9
Rohittt01 <124079221+Rohittt01@users.noreply.github.com>
2024-06-14 22:28:31 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import Generator from "./components/Generator";
+import Hero from "./components/Hero";
+import Workout from "./components/Workout";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+  <main className="min-h-screen flex flex-col bg-gradient-to-r 
+  from-slate-800 to-slate-950 text-white text-sm sm:text-base">
+
+  <Hero />
+  <Generator />
+  <Workout />
+  
+  </main>
+  );
 }
